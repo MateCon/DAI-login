d89ecf200afa2e7f2006eded03d95654f9131806
John Doe <johndoe@example.com>
2022-08-10 09:03:48 -0300

    .

--- pages/Detalle.tsx
+++ pages/Detalle.tsx
@@ -6,6 +6,7 @@ import { getPlato } from "../utils/axiosClient";
 export default function Detalle({ route, navigation }: any) {
     const [plato, setPlato] = useState<any | null>();
     const [user, setUser] = useContext(UserContext);
+    const [error, setError] = useState("");
 
     useEffect(() => {
         (async () => {
@@ -21,8 +22,23 @@ export default function Detalle({ route, navigation }: any) {
             <Image source={{ uri: plato?.image }} style={styles.image} />
             <Text>Tiempo de preparacion: {plato?.readyInMinutes} minutos</Text>
             <Text>Precio por porción: {plato?.pricePerServing}$</Text>
+            <Text>{error}</Text>
             {!user.platos.find(p => p.id === route.params.id)
-                ? <Button onPress={() => setUser({ ...user, platos: [ ...user?.platos, plato ] }) } color="green" title="Agregar a mis platos" />
+                ? <Button
+                    onPress={() => {
+                        console.log(user.platos)
+                        console.log(user.platos?.reduce((count, p) => {
+                            console.log(p)
+                            return count + p.vegan ? 0 : 1
+                        }, 0));
+                        if (user.platos?.reduce((count, p) => count + p.vegan === plato.vegan ? 1 : 0, 0) >= 2)
+                            setError(plato.vegan ? "Ya hay dos platos veganos" : "Ya hay dos platos no veganos");
+                        else 
+                            setUser({ ...user, platos: [ ...user?.platos, plato ]})
+                    }}
+                    color="green"
+                    title="Agregar a mis platos"
+                />
                 : <Button onPress={() => setUser({ ...user, platos: user.platos.filter(p => p.id !== route.params.id)}) } color="red" title="Sacar de mis platos" />}
         </View>
     )
